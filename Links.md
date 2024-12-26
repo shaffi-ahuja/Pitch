84110d2cf46bb864d24e87865b37ea06a170e830
shaffi-ahuja <shaffiahuja@Shaffis-MacBook-Air.local>
2024-12-26 20:05:55 +0530

    hero component and shadcn installation

---
+++ Links.md
@@ -0,0 +1,14 @@
+Reference links
+
+https://miro.com/app/board/uXjVLT_tMdU=/
+
+https://authjs.dev/getting-started/authentication/oauth
+
+https://authjs.dev/getting-started/providers/github
+
+https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/creating-an-oauth-app
+
+https://www.figma.com/design/TMGW6rLGene3cqHb4Kilz5/Pitch-Startup-App?node-id=62001-4701&t=7bgXvOWid1OQo6Ko-0
+
+https://ui.shadcn.com/docs/cli
+
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl ">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, 
+          <br />
+          Connect with Enterprenuers
+        </h1>
+        {/* exclamation mark for overriding */}
+        <p className="sub-heading !max-w-3xl">
+        Submit your pitches, Cote on Pitches , and Get noticed in Virtual Competitions.</p> 
+      </section>
     </>
   );
 }
