a502ed7ccaaf525f78ac1a407463bc362034b2f4
shaffi-ahuja <shaffiahuja@Shaffis-MacBook-Air.local>
2024-12-26 21:23:53 +0530

    StartupCard added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 import Image from "next/image";
 
 
@@ -7,6 +8,20 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
   //on every single nextjs page we have access to search params
 
   const query = (await searchParams).query;
+
+  const posts = [{
+    _id: 1,
+    _createdAt: new Date(),
+    views: 55,
+    author: {
+      _id: 1,
+      name:'Shaffi Ahuja'
+    },
+    image: 'https://plus.unsplash.com/premium_photo-1677094310899-02303289cadf?q=80&w=1032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    description: 'This is description',
+    category: 'Robots',
+    title: 'We Robots'
+  }]
 
   return (
     <>
@@ -19,6 +34,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{ q
         <p className="sub-heading !max-w-3xl">
           Submit your pitches, Cote on Pitches , and Get noticed in Virtual Competitions.</p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}` : `All Startups`}
+        </p>
+
+        <ul className="mt-7 card_grid ">
+          {posts?.length > 0
+            ?
+            (posts.map((post: StartupTypeCard, index: number) =>
+              <StartupCard key={post?._id} post={post} />
+            ))
+            : (<p className="no-results">No startups found</p>)}
+
+        </ul>
       </section>
     </>
   );
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
-
+  
+  //allow images from all sources in remote patterns and
+//resolve this error - [ ⨯ The requested resource "https://placehold.co/600x400" has type "image/svg+xml" but dangerouslyAllowSVG is disabled ] with dangerouslyAllowSVG: true
+  
 const nextConfig: NextConfig = {
-  /* config options here */
+
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname:'*'
+      },
+    ]
+  }
 };
 
 export default nextConfig;
